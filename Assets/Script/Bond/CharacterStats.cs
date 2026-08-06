ae6ff9a95fce3671de9c8d66be5c369f895ee27b
MatejSmoljo14-Home <1036894@student.sae.edu.au>
2026-08-06 14:49:12 +1000

    How do events work?

--- Assets/Script/Bond/CharacterStats.cs
+++ Assets/Script/Bond/CharacterStats.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace SAE.GAD176.Scripting2.Project2.CharacterBondSystem
 {
@@ -6,17 +7,32 @@ namespace SAE.GAD176.Scripting2.Project2.CharacterBondSystem
     {
         [SerializeField]
         protected string characterName = "-1";
-
         [SerializeField]
         protected int friendLevel = 0;
-
         [SerializeField]
         protected float bondExperience = 0.00f;
 
         // add events here for ChangeBondExperience()
         // BondLevelUP
 
+        public UnityEvent ChangeBondExperienceEvent = new UnityEvent();
 
+        private void OnEnable()
+        {
+            // invoke
+            ChangeBondExperienceEvent.AddListener(ChangeBondExperience);
+        }
+
+        private void OnDisable()
+        {
+            ChangeBondExperienceEvent.RemoveListener(ChangeBondExperience);
+        }
+
+        public void ChangeBondExperience()
+        {
+            bondExperience += 15; // quest reward exp
+            Debug.Log("Bond Increased" + bondExperience);
+        }
 
     }
 }
